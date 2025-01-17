1f51ff17d7465519cf172ff443ee0fd80c4d07b3
Vladimir Djurovic <vdjurovic@bitshifted.co>
2025-01-17 20:38:59 +0100

    chore(hetzner-dns): fix typos in variables (#17)

--- hetzner-dns/data.tf
+++ hetzner-dns/data.tf
@@ -5,10 +5,10 @@
 
 locals {
   declared_dns_zone_names = toset([for record in var.hcloud_dns_records : record.zone])
-  dns_zone_id_map         = { for zone in local.declared_dns_zone_names : zone => data.hetznerdns_zone.exiting_dns_zones[zone].id }
+  dns_zone_id_map         = { for zone in local.declared_dns_zone_names : zone => data.hetznerdns_zone.existing_dns_zones[zone].id }
 }
 
-data "hetznerdns_zone" "exiting_dns_zones" {
+data "hetznerdns_zone" "existing_dns_zones" {
   for_each = local.declared_dns_zone_names
   name     = each.value
 }
